6afd5adcbe547b676c035f28d6e336852ae20fdd
snowjadeai <160398147+snowjadeai@users.noreply.github.com>
2025-03-02 13:46:30 +0000

    Added input and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
+import { useState } from "react";
+
 import './App.css';
 
 function App() {
+  const [birthDate, setBirthDate] = useState("");
+  const [fortune, setFortune] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Enter your Birthdate!
+        </p>
+        <p>
+          <input
+            type="date"
+            min="1900-01-01"
+            value={birthDate}
+            onChange={(e) => setBirthDate(e.target.value)}
+          />
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {
+            (fortune === "") &&
+            <button
+              onClick={async () => {
+                setFortune("To do: call API here")
+              }}
+              disabled={birthDate === ""}
+            >
+              Show 2025 Fortune
+            </button>
+          }
+          {fortune}
         </p>
       </header>
     </div>
